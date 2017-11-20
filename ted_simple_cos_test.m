42d3224173a89424075811fb711829741d6cf749
dmacintyre <djm4912@rit.edu>
2017-11-20 14:30:14 -0500

    Timing Error Detection

---
+++ ted_simple_cos_test.m
@@ -0,0 +1,81 @@
+clear all;
+clc;
+close all;
+
+for phase = -1:0.05:1
+
+N = 10;
+INIT_PHASE_OFFSET = 0;
+SAMPLING_PHASE = phase;
+RESAMPLE_FILTER_TAPS = 2000;
+RESAMPLE_FILTER_BETA = 1;
+INPUT_SAMPLE_LENGTH = 999;
+
+
+clf;
+n = 0:1:INPUT_SAMPLE_LENGTH;
+x = cos(2*pi*(1/N)*n+(INIT_PHASE_OFFSET));
+
+% Generate ideal sample stream
+% Running it through the resampler to maintain the same group delay through
+% the filter
+ideal_stream = non_int_resample(x,0,RESAMPLE_FILTER_BETA,RESAMPLE_FILTER_TAPS);
+
+% Simulate a phase change due to a non-ideal sampling phase
+resampled_stream = non_int_resample(x,SAMPLING_PHASE,RESAMPLE_FILTER_BETA,RESAMPLE_FILTER_TAPS);
+
+% stem(ideal_stream);
+% hold on
+% stem(resampled_stream)
+
+% At what rate are we going around the circle (radians per sample)
+phase_change = (2*pi) / N;
+
+% Wrap the input signal around the unit circle at a rate of N To determine
+% magnitude and amplitude of N rate cos that is present
+current_phase = phase_change*n;
+val = ideal_stream.*exp(1j*current_phase);
+ideal_calculated_phase = angle(sum(val));
+
+% Wrap the resampled input  signal around the unit circle at a rate of N To determine
+% magnitude and amplitude of N rate cos that is present
+res_val = resampled_stream.*exp(1j*current_phase);
+resampled_calculated_phase = angle(sum(res_val));
+
+% Determine phase offset between ideal stream and resampled signal
+resample_phase_error = ideal_calculated_phase - resampled_calculated_phase;
+
+% Convert the phase offset into a number of samples delay
+% This should match the value of SAMPLING_PHASE
+samples_error = resample_phase_error / phase_change;
+
+% To reconstruct the original waveform again filter with non-integer delay
+% need to account for group delay again.
+
+y_recover = non_int_resample(resampled_stream,samples_error,RESAMPLE_FILTER_BETA,RESAMPLE_FILTER_TAPS);
+y_ideal = non_int_resample(ideal_stream,0,RESAMPLE_FILTER_BETA,RESAMPLE_FILTER_TAPS);
+
+phase
+recovered_error = sum(abs(y_recover-y_ideal))./length(n)
+unrecovered_error = sum(abs(resampled_stream-ideal_stream))./length(n)
+improvement = 20*log10(unrecovered_error/recovered_error)
+
+subplot(2,1,1)
+stem(ideal_stream(100:200))
+hold on
+stem(resampled_stream(100:200))
+legend('Ideal', 'Sampled')
+title('No Timing Error Correction')
+
+subplot(2,1,2)
+stem(y_ideal(100:200))
+hold on
+stem(y_recover(100:200))
+legend('Ideal', 'Recovered')
+title('With Timing Error Correction')
+
+error = sum(abs(y_recover-y_ideal))./length(n);
+
+pause(1);
+
+end;
